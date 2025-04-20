a7d023e6ef6e220e301b4bf547728ae7ad0d5f7f
chris <turabayochryso98@gmail.com>
2025-04-21 05:58:22 +0900

    files setting

--- worldtour/asiatours/views.py
+++ worldtour/asiatours/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Asia tour agency!")
+
+def render_template(request):
+    return render(request, 'tours/index.html')
